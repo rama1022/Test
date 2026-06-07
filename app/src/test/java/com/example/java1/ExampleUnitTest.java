004fbb7ff6687c497a855d724b8fb829ead65f0b
Itsuki Imanaka <hw22a019@oecu.jp>
2026-06-07 19:56:46 +0900

    フォルダ削除

--- app/src/test/java/com/example/java1/ExampleUnitTest.java
+++ app/src/test/java/com/example/java1/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.test2;
+package com.example.java1;
 
 import org.junit.Test;
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,3 +25,4 @@ dependencyResolutionManagement {
 rootProject.name = "test1"
 include(":app")
 include(":app")
+include(":app")
